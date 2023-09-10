849e193f23a7ec7ee4a5ce6279b31f3fd5d40543
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-09-10 13:51:02 -0400

    added a link to github repo

--- src/App.js
+++ src/App.js
@@ -2,4 +2,11 @@ import "./App.css";
 
 export default function App() {
   return <div className="App">weather app</div>;
+  <a
+    href="https://github.com/DanielleHards/react-weather-app"
+    target="_blank"
+    rel="noreferrer"
+  >
+    This code is open-sourced!
+  </a>;
 }
