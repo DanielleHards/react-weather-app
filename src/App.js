e8848bf548aa7804ca95ec0cbfba34a7063ff488
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-09-15 20:28:10 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
-  <a
-    href="https://github.com/DanielleHards/react-weather-app"
-    target="_blank"
-    rel="noreferrer"
-  >
-    This code is open-sourced!
-  </a>;
+  return (
+    <div className="App">
+      <div className="container">
+        weather app
+        <a
+          href="https://github.com/DanielleHards/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          This code is open-sourced!
+        </a>
+        ;
+      </div>
+    </div>
+  );
 }
