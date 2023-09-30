0d3e824c367a0f06dc2ee83d2c9ef381c0c49755
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-09-30 15:30:37 -0400

    Deleted celcius conversion

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,41 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-  function showC(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-  function showF(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  if (unit === "fahrenheit") {
-    return (
-      <span className="WeatherTemp">
-        <span className="temp float-left pt-2">
-          {Math.round(props.fahrenheit)}
-        </span>
-        <span className="unit float-left ">
-          °F|
-          <a href="/" onClick={showC}>
-            °C
-          </a>
-        </span>
+  return (
+    <span className="WeatherTemp">
+      <span className="temp float-left pt-2">
+        {Math.round(props.fahrenheit)}
       </span>
-    );
-  } else {
-    let celcius = ((props.fahrenheit - 32) * 5) / 9;
-    return (
-      <span className="WeatherTemp">
-        <span className="temp float-left pt-2">{Math.round(celcius)}</span>
-        <span className="unit float-left ">
-          <a href="/" onClick={showF}>
-            °F
-          </a>
-          |°C
-        </span>
-      </span>
-    );
-  }
+      <span className="unit float-left ">°F</span>
+    </span>
+  );
 }
